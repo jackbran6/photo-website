69ee036be962865f5e528af4daee9ba2d1ac539a
Jack <jack.branthwaite@gmail.com>
2021-05-29 21:42:42 +1200

    Basic start to the application

--- src/App.js
+++ src/App.js
@@ -1,12 +1,35 @@
 import "./App.css";
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+import { Home } from "./Screens/Home/home";
+import { About } from "./Screens/About/about";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <p>Fuck this shit</p>
-      </header>
-    </div>
+    <Router>
+      <div>
+        <nav>
+          <ul>
+            <li>
+              <Link to="/">Home</Link>
+            </li>
+            <li>
+              <Link to="/about">About</Link>
+            </li>
+          </ul>
+        </nav>
+
+        {/* A <Switch> looks through its children <Route>s and
+            renders the first one that matches the current URL. */}
+        <Switch>
+          <Route path="/about">
+            <About />
+          </Route>
+          <Route path="/">
+            <Home />
+          </Route>
+        </Switch>
+      </div>
+    </Router>
   );
 }
 
