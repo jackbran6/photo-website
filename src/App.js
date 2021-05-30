71fc921d832588faf95437d39cefba4959983102
Jack <jack.branthwaite@gmail.com>
2021-05-30 15:39:45 +1200

    Main pages layout created

--- src/App.js
+++ src/App.js
@@ -2,6 +2,9 @@ import "./App.css";
 import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
 import { Home } from "./Screens/Home/home";
 import { About } from "./Screens/About/about";
+import { Writing } from "./Screens/Writing/index";
+import { Photography } from "./Screens/Photography/index";
+import { Contact } from "./Screens/Contact/index";
 
 function App() {
   return (
@@ -19,27 +22,28 @@ function App() {
             <li className={"list-item"}>
               <div className={"link-container"}>
                 <Link className={"link"} to="/about">
-                  Photography
-                </Link>
-              </div>
-            </li>
-            <li className={"list-item"}>
-              <div className={"link-container"}>
-                <Link className={"link"} to="/about">
-                  Writing
-                </Link>
-              </div>
-            </li>
-            <li className={"list-item"}>
-              <div className={"link-container"}>
-                <Link className={"link"} to="/about">
                   About
                 </Link>
               </div>
             </li>
             <li className={"list-item"}>
               <div className={"link-container"}>
-                <Link className={"link"} to="/about">
+                <Link className={"link"} to="/photography">
+                  Photography
+                </Link>
+              </div>
+            </li>
+            <li className={"list-item"}>
+              <div className={"link-container"}>
+                <Link className={"link"} to="/writing">
+                  Writing
+                </Link>
+              </div>
+            </li>
+
+            <li className={"list-item"}>
+              <div className={"link-container"}>
+                <Link className={"link"} to="/contact">
                   Contact
                 </Link>
               </div>
@@ -52,6 +56,15 @@ function App() {
         <Switch>
           <Route path="/about">
             <About />
+          </Route>
+          <Route path="/photography">
+            <Photography />
+          </Route>
+          <Route path="/writing">
+            <Writing />
+          </Route>
+          <Route path="/contact">
+            <Contact />
           </Route>
           <Route path="/">
             <Home />
---
+++ src/Screens/Photography/index.js
@@ -0,0 +1,10 @@
+import React from "react";
+import "./styles.css";
+
+export const Photography = () => {
+  return (
+    <div>
+      <p>Photography</p>
+    </div>
+  );
+};
