43aefe1a51be77f17746e1ba1655b9d490ce7e75
Jack <jack.branthwaite@gmail.com>
2021-06-12 14:55:08 +1200

    Construction of the homepage begun

--- src/Screens/Home/home.js
+++ src/Screens/Home/home.js
@@ -1,9 +1,17 @@
 import React from "react";
+import "./home.css";
 
 export const Home = () => {
   return (
     <div>
-      <p>Homepage</p>
+      <div className={"overall-page"}>
+        <div className={"post-square"}></div>
+        <div className={"post-square"}></div>
+        <div className={"post-square"}></div>
+        <div className={"post-square"}></div>
+        <div className={"post-square"}></div>
+        <div className={"post-square"}></div>
+      </div>
     </div>
   );
 };
